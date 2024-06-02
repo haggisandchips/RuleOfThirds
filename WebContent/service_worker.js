0cd928311c91ce23a7a91538b5d4222ddfaf77b9
Ivor Potter <ivor.potter@gmail.com>
2024-06-02 01:24:18 +0100

    Add version update notification

--- WebContent/service_worker.js
+++ WebContent/service_worker.js
@@ -1,4 +1,22 @@
 try {
+    chrome.runtime.onInstalled.addListener((details) => {
+        const currentVersion = chrome.runtime.getManifest().version
+        const previousVersion = details.previousVersion
+        const reason = details.reason
+
+        console.log(`Previous Version: ${previousVersion }`)
+        console.log(`Current Version: ${currentVersion }`)
+
+        switch (reason) {
+            case 'update':
+                chrome.tabs.create({url:'versions/history.html'});
+                break;
+            default:
+                break;
+        }
+
+    });
+
     chrome.action.onClicked.addListener(tab => {
 
         console.log("Browser action clicked");
